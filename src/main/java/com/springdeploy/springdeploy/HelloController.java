2e419c758db1fcce2e498f437365e8c42953b83a
jorgeegonzales <jorgeegonzales10@gmail.com>
2022-09-26 10:37:45 -0500

    spring heroku app config

---
+++ src/main/java/com/springdeploy/springdeploy/HelloController.java
@@ -0,0 +1,53 @@
+package com.springdeploy.springdeploy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${app.message}")
+    String message;
+
+    @GetMapping("/hola")
+    public String holaMundo() {
+        System.out.println(message);
+        return "Hola Mundo que tal vamos!!! Hasta Luego!";
+    }
+
+    @GetMapping("/bootstrap")
+    public String bootstrap() {
+        return """
+                <!doctype html>
+                <html lang="ar" dir="rtl">
+                  <head>
+                    <!-- Required meta tags -->
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+                                
+                    <!-- Bootstrap CSS -->
+                    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.rtl.min.css" integrity="sha384-OXTEbYDqaX2ZY/BOaZV/yFGChYHtrXH2nyXJ372n2Y8abBhrqacCEe+3qhSHtLjy" crossorigin="anonymous">
+                                
+                    <title>Hola mundo!</title>
+                  </head>
+                  <body>
+                    <h>Hola mundo desde Spring Boot!</h1>
+                         <a class="btn btn-primary" href="https://www.google.com">Google</a>       
+                    <!-- Optional JavaScript; choose one of the two! -->
+                                
+                    <!-- Option 1: Bootstrap Bundle with Popper -->
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+                                
+                    <!-- Option 2: Separate Popper and Bootstrap JS -->
+                    <!--
+                    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous"></script>
+                    -->
+                  </body>
+                </html>
+                
+                """ ;
+
+    }
+}
